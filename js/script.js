00c440ac5cd6ab7520f20dc934e15adadef8c6c3
Sara <saracorsob@gmail.com>
2020-05-18 16:00:59 +0200

    Ulteriori modifiche con Javascript

--- js/script.js
+++ js/script.js
@@ -8,3 +8,9 @@ document.getElementById('song-artist-name-sinister').innerHTML = 'Christopher Yo
 document.getElementById('artist-name-sinister').innerHTML = 'Christopher Young';
 
 document.getElementById('artist-name-metal').innerHTML = 'Various Artists';
+
+document.getElementById('artist-name-quiet').innerHTML = 'Marco Beltrami';
+
+document.getElementById('artist-name-split').innerHTML = 'West Dylan Thordson';
+
+document.getElementById('artist-name-wellness').innerHTML = 'Benjamin Wallfisch';
