a378f4b74b1a07a0effa60e0c02619a1905eca36
Sara <saracorsob@gmail.com>
2020-05-18 15:56:43 +0200

    Aggiunta javascript con file esterno

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// Titoli
+document.getElementById('created-for').innerHTML = 'Created for Jane Doe';
+
+document.getElementById('popular-artist').innerHTML = 'Most Popular Artists';
+
+document.getElementById('song-artist-name-sinister').innerHTML = 'Christopher Young';
+
+document.getElementById('artist-name-sinister').innerHTML = 'Christopher Young';
+
+document.getElementById('artist-name-metal').innerHTML = 'Various Artists';
